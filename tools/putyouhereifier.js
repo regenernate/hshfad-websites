2919e700e9034bb5da584b87321bf455b6366d7a
Nathan Wheeler <nathangreenling@gmail.com>
2022-02-09 12:03:57 -0500

    added images to hs sites

--- tools/putyouhereifier.js
+++ tools/putyouhereifier.js
@@ -1,15 +1,21 @@
 const moment = require('moment');
 
+
 exports.module = function(){
 
-  let t = moment().format('h:mm');
-  let a = ( moment().format('a').toLowerCase() == "am" ) ? "morning" : ( t.split(":")[0] < 5 ) ? "afternoon" : "evening";
-  let dow = moment().format('dddd');
-  let day = moment().format('D');
-  let mon = moment().format('MMMM');
+  let the_time = moment();
+  let t = the_time.format('h:mm', 'EST');
+  let a = ( the_time.format('a').toLowerCase() == "am" ) ? "morning" : ( t.split(":")[0] < 5 ) ? "afternoon" : "evening";
+  let dow = the_time.format('dddd');
+  let day = the_time.format('D');
+  let mon = the_time.format('MMMM');
 
-  return "<p>Aha, here I see it is " + t + " on a relatively nice " + dow + " " + a + ".</p><img src='/calendar.jpg' class='narrow-image h-centered' alt='This is an image of three dots representing yesterday, today and tomorrow.' /><p>" + mon + " the " + (day - 1) +
-   " ... or maybe the " + day + ".";
+  let d_text;
+  if( day < 10 ) d_text = "early " + mon;
+  else if( day , 20 ) d_text = "mid " + mon;
+  else d_text = "late" + mon;
+
+  return "<p>Aha, here I see it is " + t + " on a relatively nice " + dow + " " + a + ".</p><img src='/calendar.jpg' class='narrow-image h-centered' alt='This is an image of three dots representing yesterday, today and tomorrow.' /><p>Sometime in " + d_text + ".";
 
 }
 /*
